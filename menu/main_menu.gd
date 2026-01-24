a82db1713ae299ad6bde980194a9de1fd112ca5a
Jun <jundi2303@gmail.com>
2026-01-24 15:39:01 +0700

    refactor

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -2,4 +2,4 @@ extends PanelContainer
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://gameplay.tscn")
+	get_tree().change_scene_to_file("res://gameplay/gameplay.tscn")
