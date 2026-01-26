a55b59c669108d6c8caf696125703e3eae19d1d2
Jun <jundi2303@gmail.com>
2026-01-26 11:54:05 +0700

    main menu

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,6 +1,10 @@
-extends PanelContainer
+extends Panel
 
 
 func _on_play_button_pressed() -> void:
 	AudioPlayer.game_started = true
 	get_tree().change_scene_to_file("res://gameplay/gameplay.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
